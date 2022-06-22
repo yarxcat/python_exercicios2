3f642cee76336394aeea9ae6cfc3df6c5f7862e9
yarxcat <107942788+yarxcat@users.noreply.github.com>
2022-06-22 15:03:55 -0300

    Update and rename 0026.py to manipulando_uma_frase.py

--- manipulando_uma_frase.py
+++ manipulando_uma_frase.py
@@ -1,3 +1,4 @@
+#manipulando uma string
 frase = str(input('Digite uma frase: ')).upper().strip()
 print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
 print('A primeira letra A apreceu na posição {}'.format(frase.find('A')+1))
