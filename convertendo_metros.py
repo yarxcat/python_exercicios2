8ebdbc4b0ea5d3388f821c2f77fce8960821a6be
yarxcat <107942788+yarxcat@users.noreply.github.com>
2022-06-26 10:58:18 -0300

    Update and rename 007.py to convertendo_metros.py

---
+++ convertendo_metros.py
@@ -0,0 +1,5 @@
+#convertendo m para cm e mm
+medida = float(input('Uma distancia em metros: '))
+cm = medida*100
+mm = medida*1000
+print('A medida de {}m corresponde a {}cm e {}mm.'.format(medida, cm, mm))
